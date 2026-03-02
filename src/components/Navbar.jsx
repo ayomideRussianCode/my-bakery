7f6d488c09540725b3227159f92fcbf3535c922a
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-03-02 19:12:12 +0100

    navbar and hero component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="flex w-full items-center justify-between bg-white px-4 py-4 sm:px-6 lg:px-8 shadow-sm ">
+        <div className="px-4  space-x-8 sm:px-6 lg:px-8 flex ">
+          <img className="h-10 w-auto" src="../../public/Logo.svg" alt="Logo" />
+        </div>
+        <div className="lg:space-x-12 ">
+          <ul className="text-black">
+            <a className="no-underline" href="">
+              FAQs
+            </a>
+            <a className="no-underline" href="">
+              Log in
+            </a>
+            <a
+              className="no-underline rounded-xl shadow-md px-5 py-2 bg-[#ce0639]"
+              href=""
+            >
+              Create Account
+            </a>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export const Hero = () => {
+    return (
+      <>
+        <div>
+          <img
+            className="h-50 w-full object-cover sm:h-60 md:h-70 lg:h-80 linear-gradient-to-l from-[#5f5656] to-[#7e7777]"
+            src="../../public/Bakery image 1 .png"
+            alt=""
+          />
+          <h2>Take your bakery to the next level today.</h2>
+        </div>
+      </>
+    );
+}
+export default Navbar;
