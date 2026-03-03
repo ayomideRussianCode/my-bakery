1532d2f84c5bf2b718e13df13546354aef960af4
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-03-03 12:02:01 +0100

    hero section img fixed for all screen sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
   return (
     <>
-      <header className="flex w-auto bg-[#ffffff] z-40 items-center justify-between mx-8 my-2 sm:px-6 ">
+      <header className="flex w-auto bg-[#ffffff] items-center justify-between mx-8 my-2 sm:px-6 ">
         <div className="sm:space-x-8 sm:px-6 lg:px-8 flex ">
           <img className="h-8" src="../../public/Logo.svg" alt="Logo" />
         </div>
@@ -10,19 +10,19 @@ const Navbar = () => {
           <nav className=" hidden md:flex items-center px-5 py-2 space-x-8">
             <a
               className="no-underline font-productSans text-[#404c60] text-xs"
-              href=""
+              href="#"
             >
               FAQs
             </a>
             <a
               className="no-underline font-productSans text-xs text-[#404c60]"
-              href=""
+              href="#"
             >
               Log in
             </a>
             <a
-              className="no-underline font-productSans text-xs text-[#fff] rounded-lg shadow-md px-2 py-2 bg-[#ce0639]"
-              href=""
+              className="no-underline font-productSans text-xs text-[#fff] rounded-lg shadow-md px-4 py-2 bg-[#ce0639]"
+              href="#"
             >
               Create Account
             </a>
@@ -38,7 +38,7 @@ export const Hero = () => {
       <>
         <div>
           <img
-            className="h-50 object-cover sm:h-60 md:h-70 lg:h-80"
+            className="h-50 object-cover sm:h-60 md:h-70 lg:h-full w-full"
             src="../../public/Bakery image 1 .png"
             alt=""
           />
