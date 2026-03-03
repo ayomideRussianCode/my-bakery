ce1a70111460b819aac990a6a99378b3781562ab
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-03-03 10:24:26 +0100

    tailwind set right

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@
 const Navbar = () => {
   return (
     <>
-      <nav className="flex w-full items-center justify-between bg-white px-4 py-4 sm:px-6 lg:px-8 shadow-sm ">
-        <div className="px-4  space-x-8 sm:px-6 lg:px-8 flex ">
+      <header className="flex w-full items-center justify-between bg-white px-4 py-4 sm:px-6 lg:px-8 shadow-sm ">
+        <div className="mx-[24] sm:space-x-8 sm:px-6 lg:px-8 flex ">
           <img className="h-10 w-auto" src="../../public/Logo.svg" alt="Logo" />
         </div>
-        <div className="lg:space-x-12 ">
-          <ul className="text-black">
+        <div className="text-black ">
+          <nav className="flex items-center space-x-8">
             <a className="no-underline" href="">
               FAQs
             </a>
@@ -15,14 +15,14 @@ const Navbar = () => {
               Log in
             </a>
             <a
-              className="no-underline rounded-xl shadow-md px-5 py-2 bg-[#ce0639]"
+              className="no-underline rounded-xl shadow-md px-4 py-2 bg-[#ce0639]"
               href=""
             >
               Create Account
             </a>
-          </ul>
+          </nav>
         </div>
-      </nav>
+      </header>
     </>
   );
 };
@@ -36,7 +36,9 @@ export const Hero = () => {
             src="../../public/Bakery image 1 .png"
             alt=""
           />
-          <h2>Take your bakery to the next level today.</h2>
+          <div className="">
+           
+          </div>
         </div>
       </>
     );
---
+++ src/components/Testing.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Testing = () => {
+  return (
+    <div className="bg-red-500 p-10 text-white rounded-full">Tailwind Test</div>
+  );
+};
+
+export default Testing;
